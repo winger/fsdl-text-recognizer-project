605b98bae34ceed72acff55404d7c7953d9a0d71
winger <winger@local>
2018-08-03 23:55:28 +0000

    Labs 2 & 3

--- lab2/text_recognizer/networks/lenet.py
+++ lab2/text_recognizer/networks/lenet.py
@@ -7,9 +7,27 @@ from tensorflow.keras.models import Sequential, Model
 
 def lenet(input_shape: Tuple[int, ...], output_shape: Tuple[int, ...]) -> Model:
     num_classes = output_shape[0]
+    print(input_shape, output_shape)
 
     ##### Your code below (Lab 2)
-
+    model = Sequential()
+    # Reshape as appropriate
+    if len(input_shape) < 3:
+        model.add(Lambda(lambda x: tf.expand_dims(x, -1), input_shape=input_shape))
+        input_shape = (1,)
+    model.add(Conv2D(input_shape=input_shape, filters=64, kernel_size=3, activation='relu'))
+    model.add(MaxPooling2D())
+    model.add(Dropout(0.2))
+    model.add(Conv2D(filters=64, kernel_size=3, activation='relu'))
+    model.add(MaxPooling2D())
+    model.add(Dropout(0.2))
+    model.add(Flatten())
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(num_classes, activation='softmax'))
+    
     ##### Your code above (Lab 2)
 
     return model
